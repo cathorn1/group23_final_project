a29552474999d85d4140d0291383a48f765e2bc1
CliftonT <cathorn1@uncc.edu>
2021-11-17 17:36:49 -0500

    nc fin

--- Final_Project/NC_Annual_refugees.py
+++ Final_Project/NC_Annual_refugees.py
@@ -5,21 +5,8 @@ import plotly.graph_objs as go
 # Load CSV file from Datasets folder
 df = pd.read_csv('Datasets/annual_refugee_data.csv')
 
-# Removing empty spaces from State column to avoid errors
-# df = df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
-
-# Creating unrecovered column
-# df['Unrecovered'] = df['Confirmed'] - df['Deaths'] - df['Recovered']
-
-# Creating sum of number of cases group by Country Column
-# new_df = df.groupby(['Country']).agg(
-#     {'Confirmed': 'sum', 'Deaths': 'sum', 'Recovered': 'sum', 'Unrecovered': 'sum'}).reset_index()
-
 df = df[(df['state'] == 'North Carolina')]
 
-# df = df.groupby(['state'])['annualtotal'].sum().reset_index()
-
-# Preparing data
 trace1 = [go.Bar(x=df['year'], y=df['people'])]
 
 # Preparing layout
