c6a51e6f80e358745f7013111ff94cd9df1e49dc
CliftonT <cathorn1@uncc.edu>
2021-11-17 20:24:43 -0500

    nc interactive

--- Final_Project/Asylum.py
+++ Final_Project/Asylum.py
@@ -14,8 +14,6 @@ df = df[(df['Year'] == "2020")]
 df = df[(df['Population Type'] == "ASY")]
 df = df[(df['Total'] >= 5000)]
 
-
-
 # Preparing data ---- create traces for male and female total
 trace1 = go.Bar(x=df['Country of Origin Name'], y=df['Total'],  name='Total')
 data = [ trace1]
